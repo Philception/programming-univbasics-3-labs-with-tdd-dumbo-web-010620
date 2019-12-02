5058125a6e10890f70fdbba2c24115d020b1e5c3
Phillip Henry III <philtrader71@gmail.com>
2019-12-02 18:04:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,4 @@ first_number = 40
 second_number = 25
 sum = first_number + second_number
 difference = first_number - second_number
-# Add your variables here
+product = first_number * second_number 
